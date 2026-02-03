3a0b113f197a854b5e4e5c8be8a1d79484f61be1
Kaladhar Royal <kaladharroyal@gmail.com>
2026-02-03 11:25:25 +0530

    updated vercel link

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors({
   origin: [
     "http://localhost:5173", // Vite local
-    "https://student-tracker-aw6t.vercel.app" // production frontend
+    "https://campus-loop-jmnh.vercel.app" // production frontend
   ],
   credentials: true
 }));
